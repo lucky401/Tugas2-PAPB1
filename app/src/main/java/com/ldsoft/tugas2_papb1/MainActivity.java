f9ca29255d6cb512df6624c8fe4c36f88014ed93
Lucky Dewa Satria <luckydewa123@gmail.com>
2020-09-21 19:37:33 +0700

    Initial commit

---
+++ app/src/main/java/com/ldsoft/tugas2_papb1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ldsoft.tugas2_papb1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_button_toast, Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
